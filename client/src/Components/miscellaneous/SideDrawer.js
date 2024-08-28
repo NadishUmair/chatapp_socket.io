03fb279ac21aca8a635dde176a959425a6175662
NadishUmair <nadishumair403@gmail.com>
2024-08-28 23:15:03 +0500

    navbar

--- client/src/Components/miscellaneous/SideDrawer.js
+++ client/src/Components/miscellaneous/SideDrawer.js
@@ -1,22 +1,61 @@
-import { Box, Button, Text, Tooltip } from '@chakra-ui/react'
-import React from 'react'
+import { Avatar, AvatarBadge, Box, Button, Menu, MenuButton, MenuItem, MenuList, Text, Tooltip } from '@chakra-ui/react';
+import React from 'react';
+import { CiSearch } from 'react-icons/ci';
+import { FaBell } from 'react-icons/fa';
+import { IoIosArrowDown } from 'react-icons/io';
+import { useChatState } from '../../Context/ChatProvider';
 
 const SideDrawer = () => {
+  const { user }=useChatState()
   return (
-    <>
-    <Box>
-
-      <Tooltip label="Search Users to chat" hasArrow placement='bottom-end'>
+    <Box
+      display="flex"
+      justifyContent="space-between"
+      alignItems="center"
+      bg="white"
+      w="100%"
+      p="5px 10px"
+      borderWidth="5px"
+    >
+      <Tooltip label="Search Users to chat" hasArrow placement="bottom-end">
         <Button>
-
-         <Text>
-          Search
-         </Text>
+          <CiSearch />
+          <Text display={{ base: 'none', md: 'flex' }} px="4">
+            Search
+          </Text>
         </Button>
       </Tooltip>
-    </Box>
-    </>
-  )
-}
 
-export default SideDrawer
+      <Text fontSize="2xl" fontFamily="sans-serif">
+        Talk-A-Tive
+      </Text>
+
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+     
+       
+      >
+        <Menu>
+          <MenuButton p={1}>
+            <FaBell fontSize="25px" m={1} />
+          </MenuButton>
+        </Menu>
+        <Menu>
+          <MenuButton as={Button} rightIcon={<IoIosArrowDown />}>
+            <Avatar size="sm" cursor="pointer" name={user.name}
+             src={user.avatar}
+            />
+          </MenuButton>
+          <MenuList>
+            <MenuItem>My Profile</MenuItem>
+            <MenuItem>Logout</MenuItem>
+          </MenuList>
+        </Menu>
+      </Box>
+    </Box>
+  );
+};
+
+export default SideDrawer;
